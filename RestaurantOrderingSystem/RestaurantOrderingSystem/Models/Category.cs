54a9b50be761d53ccd91e17ee374308f521c58b6
Weiss <weisslipe@gmail.com>
2022-05-07 23:47:28 -0300

    Adding Category

---
+++ RestaurantOrderingSystem/RestaurantOrderingSystem/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantOrderingSystem.Models
+{
+    [Table("categories")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(60)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- RestaurantOrderingSystem/RestaurantOrderingSystem/Models/DatabaseContext.cs
+++ RestaurantOrderingSystem/RestaurantOrderingSystem/Models/DatabaseContext.cs
@@ -5,6 +5,7 @@ namespace RestaurantOrderingSystem.Models
     public class DatabaseContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options) { }
