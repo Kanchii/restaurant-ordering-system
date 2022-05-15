af2d276935ba3073d2dd451f4be533011a58629f
Kanchii <weisslipe@gmail.com>
2022-05-15 12:15:49 -0300

    Updated database with new foreign key

---
+++ RestaurantOrderingSystem/RestaurantOrderingSystem/Migrations/20220515151148_AddCategoryAsForeignKeyForProductModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantOrderingSystem.Migrations
+{
+    public partial class AddCategoryAsForeignKeyForProductModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_CategoryId",
+                table: "products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "products");
+        }
+    }
+}
